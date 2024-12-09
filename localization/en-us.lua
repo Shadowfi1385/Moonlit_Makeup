57f062878c8b6874aebc3b872ee07ab6ab1ce27c
Shadowfi1385 <shadowfi1385@gmail.com>
2024-12-09 19:00:55 +0330

    wa
    wa

---
+++ localization/en-us.lua
@@ -0,0 +1,86 @@
+return {
+    misc = {
+          quips={
+              dq_1={
+                  "Yikes! I hope you",
+                  "have a few tricks",
+                  "up your sleeve for",
+                  "this final challenge!",
+              },
+              lq_1={
+                  "Maybe Go Fish",
+                  "is more our",
+                  "speed...",
+              },
+              lq_10={
+                  "What a flop!",
+              },
+              lq_2={
+                  "We folded like",
+                  "a cheap suit!",
+              },
+              lq_3={
+                  "Time for us",
+                  "to shuffle off",
+                  "and try again!",
+              },
+              lq_4={
+                  "You know what",
+                  "they say, the",
+                  "house always wins!",
+              },
+              lq_5={
+                  "Looks like we",
+                  "found out who",
+                  "the real Joker is!",
+              },
+              lq_6={
+                  "Oh no, were you",
+                  "bluffing too?",
+              },
+              lq_7={
+                  "Looks like the",
+                  "joke's on us!",
+              },
+              lq_8={
+                  "If I had hands",
+                  "I would have",
+                  "covered my eyes!",
+              },
+              lq_9={
+                  "I'm literally",
+                  "a fool, what's",
+                  "your excuse?",
+              },
+              wq_1={
+                  "You Aced it!",
+              },
+              wq_2={
+                  "You dealt with",
+                  "that pretty well!",
+              },
+              wq_3={
+                  "Looks like you",
+                  "weren't bluffing!",
+              },
+              wq_4={
+                  "Too bad these",
+                  "chips are all",
+                  "virtual...",
+              },
+              wq_5={
+                  "Looks like I've",
+                  "taught you well!",
+              },
+              wq_6={
+                  "You made some",
+                  "heads up plays!",
+              },
+              wq_7={
+                  "Good thing",
+                  "I didn't bet",
+                  "against you!",
+              },
+    }
+  }
+}
